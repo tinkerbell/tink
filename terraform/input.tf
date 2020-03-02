f48827cdc35f1a66effa8fa2ae3d03d0f88e32bc
parauliya <aman@infracloud.io>
2020-03-02 19:57:11 +0530

    complete setup with provisioner and worker machine

--- terraform/input.tf
+++ terraform/input.tf
@@ -1,11 +1,21 @@
 variable "provisioner_ip" {
   type = string
-  default     = "192.168.2.1"
+  default     = "192.168.1.1"
 }
 
 variable "cidr" {
     type = number
-    default = 28
+    default = 29
+}
+
+variable "last_ip" {
+  type = string
+  default     = "192.168.1.7"
+}
+
+variable "netmask" {
+  type = string
+  default     = "255.255.255.248"
 }
 
 // This variable will be removed once the quay repository will be opensourced
@@ -18,6 +28,18 @@ variable "quay_user" {
 variable "quay_pass" {
     type = string
     default = "quay_password"
+}
+
+// This variable will be removed once the quay repository will be opensourced
+variable "git_user" {
+    type = string
+    default = "github_username"
+}
+
+// This variable will be removed once the quay repository will be opensourced
+variable "git_pass" {
+    type = string
+    default = "github_password"
 }
 
 variable "rover_registry_user" {
