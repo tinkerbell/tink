43ae0d026bf4c0e4565f5de7e9c2ccaabb5e6a33
Nathan Goulding <nathan.goulding@gmail.com>
2020-03-31 15:08:59 -0400

    remove e2e tests

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -1,22 +1,22 @@
-package e2e
-
-import (
+//package e2e
+//
+//import (
 //	"os"
-//	"testing"
 //	"time"
-
-	"github.com/tinkerbell/tink/client"
-	"github.com/tinkerbell/tink/protos/workflow"
-	"github.com/tinkerbell/tink/test/framework"
-	"github.com/sirupsen/logrus"
-)
-
-var log *logrus.Logger = framework.Log
-
-func TestMain(m *testing.M) {
-	log.Infoln("########Creating Setup########")
-//	err := framework.StartStack()
+//	"testing"
+//
+//	"github.com/tinkerbell/tink/client"
+//	"github.com/tinkerbell/tink/protos/workflow"
+//	"github.com/tinkerbell/tink/test/framework"
+//	"github.com/sirupsen/logrus"
+//)
+//
+//var log *logrus.Logger = framework.Log
+//
+//func TestMain(m *testing.M) {
+//	log.Infoln("########Creating Setup########")
 //	time.Sleep(10 * time.Second)
+//	err := framework.StartStack()
 //	if err != nil {
 //		os.Exit(1)
 //	}
@@ -45,28 +45,28 @@ func TestMain(m *testing.M) {
 //	}
 //	log.Infoln("########Setup removed########")
 //	os.Exit(status)
-}
-
-var testCases = map[string]struct {
-	target   string
-	template string
-	workers  int64
-	expected workflow.ActionState
-	ephData  string
-}{
-	"testWfWithWorker": {"target_1.json", "sample_1", 1, workflow.ActionState_ACTION_SUCCESS, `{"action_02": "data_02"}`},
-	"testWfTimeout":    {"target_1.json", "sample_2", 1, workflow.ActionState_ACTION_TIMEOUT, `{"action_01": "data_01"}`},
-	//"testWfWithMultiWorkers": {"target_1.json", "sample_3", 2, workflow.ActionState_ACTION_SUCCESS, `{"action_01": "data_01"}`},
-}
-
-var runTestMap = map[string]func(t *testing.T){
-	"testWfWithWorker": TestWfWithWorker,
-	"testWfTimeout":    TestWfTimeout,
-	//"testWfWithMultiWorkers": TestWfWithMultiWorkers,
-}
-
-func TestE2E(t *testing.T) {
-	for key, val := range runTestMap {
-		t.Run(key, val)
-	}
-}
+//}
+//
+//var testCases = map[string]struct {
+//	target   string
+//	template string
+//	workers  int64
+//	expected workflow.ActionState
+//	ephData  string
+//}{
+//	"testWfWithWorker": {"target_1.json", "sample_1", 1, workflow.ActionState_ACTION_SUCCESS, `{"action_02": "data_02"}`},
+//	"testWfTimeout":    {"target_1.json", "sample_2", 1, workflow.ActionState_ACTION_TIMEOUT, `{"action_01": "data_01"}`},
+//	//"testWfWithMultiWorkers": {"target_1.json", "sample_3", 2, workflow.ActionState_ACTION_SUCCESS, `{"action_01": "data_01"}`},
+//}
+//
+//var runTestMap = map[string]func(t *testing.T){
+//	"testWfWithWorker": TestWfWithWorker,
+//	"testWfTimeout":    TestWfTimeout,
+//	//"testWfWithMultiWorkers": TestWfWithMultiWorkers,
+//}
+//
+//func TestE2E(t *testing.T) {
+//	for key, val := range runTestMap {
+//		t.Run(key, val)
+//	}
+//}
