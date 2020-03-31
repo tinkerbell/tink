d55766fbb41a7ac183b2fa5d9c9e41aa4e12dd05
parauliya <aman@infracloud.io>
2020-03-31 17:17:45 +0530

    Add install docker and docker-compose

---
+++ test/install_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Get docker and docker-compose
+curl -L get.docker.com | bash
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
