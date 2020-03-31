36d89e056500b2d17bc4acaefaf588be0428c42c
parauliya <aman@infracloud.io>
2020-03-31 18:14:24 +0530

    test with alpine image

--- test/install_docker.sh
+++ test/install_docker.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get docker and docker-compose
-apt install curl
-curl -L get.docker.com | bash
+apk add curl
+curl -L get.docker.com | sh
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
