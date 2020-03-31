79295bfa3295cbdf647abd0e0f4a01472f199b1e
parauliya <aman@infracloud.io>
2020-03-31 18:01:32 +0530

    Add ubuntu image for test

--- test/install_docker.sh
+++ test/install_docker.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # Get docker and docker-compose
 apt install curl
+curl -L get.docker.com | bash
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
