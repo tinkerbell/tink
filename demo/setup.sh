47a833d2dc66fb0a744bac49c5120ce49c529c48
Kelly <kelly@packet.com>
2020-04-06 14:28:05 -0400

    moving demo branch

---
+++ demo/setup.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+source tinkenv
+
+function initial_install() {
+  # install Go
+  wget https://dl.google.com/go/go1.13.9.linux-amd64.tar.gz
+  tar -C /usr/local -xzf go1.13.9.linux-amd64.tar.gz go/
+  rm go1.13.9.linux-amd64.tar.gz
+
+  # set GOPATH
+  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+  echo 'export GOPATH=$GOPATH:$HOME/go' >> ~/.bashrc
+  echo 'export PATH=$PATH:$GOPATH' >> ~/.bashrc
+  source ~/.bashrc
+
+  # install Docker and Docker Compose
+  curl -L get.docker.com | bash
+  curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+}
+
+function setup_network() {
+    network_interface=$(grep auto /etc/network/interfaces | tail -1 | cut -d ' ' -f 2)
+    echo "This is the network interface" $network_interface
+
+    grep "$HOST_IP" /etc/network/interfaces
+    if [[ $? -eq 1 ]]
+    then
+        declare bond=$(cat /etc/network/interfaces | tail -1)
+        sed -i -e "s/$bond//g" /etc/network/interfaces
+        sed -i "/$network_interface inet \(manual\|dhcp\)/c\\iface $network_interface inet static\n    address $HOST_IP\n    netmask $NETMASK\n    broadcast $BROAD_IP" /etc/network/interfaces
+    fi
+    ifdown  $network_interface
+    ifup  $network_interface
+
+    sudo ip addr add $NGINX_IP/$IP_CIDR dev $network_interface
+}
+
+function setup_osie_with_nginx() {
+    mkdir -p /packet/nginx/misc/osie/current
+    mkdir -p /packet/nginx/workflow/
+    cd /packet/nginx/workflow/
+    wget https://raw.githubusercontent.com/tinkerbell/osie/master/installer/workflow-helper.sh
+    wget https://raw.githubusercontent.com/tinkerbell/osie/master/installer/workflow-helper-rc
+    chmod +x workflow-helper.sh
+
+    cd /tmp
+    curl 'https://packet-osie-uploads.s3.amazonaws.com/osie-v19.10.23.00-n=55,c=be58d67,b=master.tar.gz' -o osie.tar.gz
+    tar -zxvf osie.tar.gz
+    cd /tmp/'osie-v19.10.23.00-n=55,c=be58d67,b=master'
+    cp -r grub /packet/nginx/misc/osie/current/
+    cp modloop-x86_64 /packet/nginx/misc/osie/current/
+#    cp initramfs-x86_64 /packet/nginx/misc/osie/current/
+#    cp vmlinuz-x86_64 /packet/nginx/misc/osie/current/
+    rm /tmp/'osie-v19.10.23.00-n=55,c=be58d67,b=master' -rf
+
+    cd ~/go/src/github.com/tinkerbell/tink/demo/misc/osie
+    cp initramfs-x86_64 /packet/nginx/misc/osie/current/
+    cp vmlinuz-x86_64 /packet/nginx/misc/osie/current/
+
+    cd /packet/nginx/misc/osie/current
+    curl 'https://packet-osie-uploads.s3.amazonaws.com/ubuntu_18_04.tar.gz' -o ubuntu_18_04.tar.gz
+    tar -zxvf ubuntu_18_04.tar.gz
+    rm ubuntu_18_04.tar.gz
+}
+function build_and_setup_certs () {
+    sudo apt-get install -y wget ca-certificates
+
+    cd ~/go/src/github.com/tinkerbell/tink
+    grep "$HOST_IP" tls/server-csr.in.json
+    if [[ $? -eq 1 ]]
+    then
+        sed -i -e "s/localhost\"\,/localhost\"\,\n    \"$HOST_IP\"\,/g" tls/server-csr.in.json
+    fi
+    # build the certificates
+    docker-compose up --build -d certs
+    sleep 5
+    # update host to trust registry certificate
+    mkdir -p /etc/docker/certs.d/$HOST_IP
+    cp certs/ca.pem /etc/docker/certs.d/$HOST_IP/ca.crt
+
+    # copy certificate in tinkerbell
+    cp certs/ca.pem /packet/nginx/workflow/ca.pem
+}
+
+function build_registry_and_update_worker_image() {
+    # build private registry
+    docker-compose up --build -d registry
+    sleep 5
+
+    # pull the worker image and push into private registry
+    docker pull quay.io/tinkerbell/tink-worker:latest
+    docker tag quay.io/tinkerbell/tink-worker:latest $HOST_IP/tink-worker:latest
+
+    # login to private registry and push the worker image
+    docker login -u=$TINKERBELL_REGISTRY_USER -p=$TINKERBELL_REGISTRY_PASS $HOST_IP
+    docker push $HOST_IP/tink-worker:latest
+}
+
+function start_docker_stack() {
+
+    docker-compose up --build -d db
+    sleep 5
+    docker-compose up --build -d cacher
+    sleep 5
+    docker-compose up --build -d tink-server
+    sleep 5
+    docker-compose up --build -d nginx
+    sleep 5
+    docker-compose up --build -d hegel
+    sleep 5
+    docker-compose up --build -d boots
+    sleep 5
+    docker-compose up --build -d kibana
+    sleep 2
+    docker-compose up --build -d tink-cli
+}
+
+function update_iptables() {
+    iptables -t nat -I POSTROUTING -s $HOST_IP/$IP_CIDR  -j MASQUERADE
+    iptables -I FORWARD -d $HOST_IP/$IP_CIDR  -j ACCEPT
+    iptables -I FORWARD -s $HOST_IP/$IP_CIDR  -j ACCEPT
+}
+
+initial_install;
+setup_network;
+setup_osie_with_nginx;
+build_and_setup_certs;
+build_registry_and_update_worker_image;
+start_docker_stack;
+update_iptables;
---
+++ demo/terraform/main.tf
@@ -0,0 +1,62 @@
+# Configure the Packet Provider.
+provider "packet" {
+  auth_token = var.auth_token
+}
+
+# Declare your project ID
+locals {
+  project_id = var.project_id
+}
+
+# Create a new VLAN in datacenter "ewr1"
+resource "packet_vlan" "provisioning-vlan" {
+  description = "provisioning-vlan"
+  facility    = "sjc1"
+  project_id  = local.project_id
+}
+
+# Create a device and add it to tf_project_1
+resource "packet_device" "tf-provisioner" {
+  hostname         = "tf-provisioner"
+  plan             = "t3.small.x86"
+  facilities       = ["sjc1"]
+  operating_system = "ubuntu_18_04"
+  billing_cycle    = "hourly"
+  project_id       = local.project_id
+  network_type     = "hybrid"
+}
+
+# Create a device and add it to tf_project_1
+resource "packet_device" "tf-worker" {
+  hostname         = "tf-worker"
+  plan             = "c2.medium.x86"
+  facilities       = ["sjc1"]
+  operating_system = "custom_ipxe"
+  ipxe_script_url  = "https://boot.netboot.xyz"
+  always_pxe       = "true"
+  billing_cycle    = "hourly"
+  project_id       = local.project_id
+  network_type     = "layer2-individual"
+}
+
+# Attach VLAN to provisioner
+resource "packet_port_vlan_attachment" "provisioner" {
+  device_id = packet_device.tf-provisioner.id
+  port_name = "eth1"
+  vlan_vnid = packet_vlan.provisioning-vlan.vxlan
+}
+
+# Attach VLAN to worker
+resource "packet_port_vlan_attachment" "worker" {
+  device_id = packet_device.tf-worker.id
+  port_name = "eth0"
+  vlan_vnid = packet_vlan.provisioning-vlan.vxlan
+}
+
+output "provisioner_ip" {
+  value = "${packet_device.tf-provisioner.network[0].address}"
+}
+
+output "worker_mac_addr" {
+  value = "${packet_device.tf-worker.ports[1].mac}"
+}
