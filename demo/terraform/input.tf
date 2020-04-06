8506ed2224e9e8b1025856b1b690f989193dd247
Kelly <kelly@packet.com>
2020-04-06 14:32:47 -0400

    moving pt2

---
+++ demo/terraform/input.tf
@@ -0,0 +1,9 @@
+variable "auth_token" {
+  description = "Packet api token"
+  default     = "packet_api_token"
+}
+
+variable "project_id" {
+  description = "Project ID"
+  default     = "project_id"
+}
