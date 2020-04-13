bdac48cb3bb97ac705a5f589ca81750ecd819ebb
parauliya <aman@infracloud.io>
2020-04-13 19:24:44 +0530

    Fix for issue#61 :  Restarting tink will not reset the database and hardware data will persist.

--- db/db.go
+++ db/db.go
@@ -19,12 +19,6 @@ func ConnectDB(lg log.Logger) *sql.DB {
 		logger.Error(err)
 		panic(err)
 	}
-	if err := truncate(db); err != nil {
-		if pqErr := Error(err); pqErr != nil {
-			logger.With("detail", pqErr.Detail, "where", pqErr.Where).Error(err)
-		}
-		panic(err)
-	}
 	return db
 }
 
@@ -34,24 +28,6 @@ func Error(err error) *pq.Error {
 		return pqErr
 	}
 	return nil
-}
-
-func truncate(db *sql.DB) error {
-	tx, err := db.BeginTx(context.Background(), &sql.TxOptions{Isolation: sql.LevelSerializable})
-	if err != nil {
-		return errors.Wrap(err, "BEGIN transaction")
-	}
-
-	_, err = tx.Exec("TRUNCATE hardware")
-	if err != nil {
-		return errors.Wrap(err, "TRUNCATE")
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return errors.Wrap(err, "TRUNCATE")
-	}
-	return err
 }
 
 func get(ctx context.Context, db *sql.DB, query string, args ...interface{}) (string, error) {
