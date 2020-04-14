892a4ee7580dbb27c1af6b13841b7ddf012ac951
Kelly <kelly@packet.com>
2020-04-14 14:18:25 -0400

    added endpoints for crud operations

--- http-server/http-server.go
+++ http-server/http-server.go
@@ -3,19 +3,28 @@ package httpserver
 import (
 	"bytes"
 	"context"
+	"crypto/x509"
 	"encoding/json"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"net/http"
 	"runtime"
 	"time"
 
+	grpcRuntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/packethost/pkg/log"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/tinkerbell/tink/protos/hardware"
+	"github.com/tinkerbell/tink/protos/target"
+	"github.com/tinkerbell/tink/protos/template"
+	"github.com/tinkerbell/tink/protos/workflow"
 )
 
 var (
 	gitRev         = "unknown"
 	gitRevJSON     []byte
+	grpcListenAddr = "localhost:42113"
 	httpListenAddr = ":42114"
 	startTime      = time.Now()
 	logger         log.Logger
@@ -24,6 +33,35 @@ var (
 // SetupHTTP setup and return an HTTP server
 func SetupHTTP(ctx context.Context, lg log.Logger, certPEM []byte, modTime time.Time, errCh chan<- error) {
 	logger = lg
+
+	cp := x509.NewCertPool()
+	ok := cp.AppendCertsFromPEM(certPEM)
+	if !ok {
+		logger.Error(errors.New("parse cert"))
+	}
+
+	creds := credentials.NewClientTLSFromCert(cp, "")
+
+	mux := grpcRuntime.NewServeMux()
+
+	dialOpts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+	err := hardware.RegisterHardwareServiceHandlerFromEndpoint(ctx, mux, grpcListenAddr, dialOpts)
+	if err != nil {
+		logger.Error(err)
+	}
+	err = target.RegisterTargetHandlerFromEndpoint(ctx, mux, grpcListenAddr, dialOpts)
+	if err != nil {
+		logger.Error(err)
+	}
+	err = template.RegisterTemplateHandlerFromEndpoint(ctx, mux, grpcListenAddr, dialOpts)
+	if err != nil {
+		logger.Error(err)
+	}
+	err = workflow.RegisterWorkflowSvcHandlerFromEndpoint(ctx, mux, grpcListenAddr, dialOpts)
+	if err != nil {
+		logger.Error(err)
+	}
+
 	http.HandleFunc("/cert", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeContent(w, r, "server.pem", modTime, bytes.NewReader(certPEM))
 	})
@@ -31,6 +69,7 @@ func SetupHTTP(ctx context.Context, lg log.Logger, certPEM []byte, modTime time.
 	setupGitRevJSON()
 	http.HandleFunc("/version", versionHandler)
 	http.HandleFunc("/_packet/healthcheck", healthCheckHandler)
+	http.Handle("/", mux)
 
 	srv := &http.Server{
 		Addr: httpListenAddr,
