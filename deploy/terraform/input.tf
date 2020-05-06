87c2d85d0412aac5853651c4f985907929f2dc64
Kelly Deng <kelly@packet.com>
2020-05-06 15:39:22 -0400

    added terraform into deploy dir (#96)
    * added terraform and moved everything setup into the deploy dir
    
    * moved back setup.sh
    
    * changed default worker plan

---
+++ deploy/terraform/input.tf
@@ -0,0 +1,9 @@
+variable "packet_api_token" {
+  description = "Packet user api token"
+  default     = "<packet_user_api_token>"
+}
+
+variable "project_id" {
+  description = "Project ID"
+  default     = "<project_id>"
+}
