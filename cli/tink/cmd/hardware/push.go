c03d4b48daae1db16fd5b4279079cb0fec981c57
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2020-05-06 12:51:31 +0530

    using flag to receive data
    Signed-off-by: Gaurav Gahlot <gaurav.gahlot19@gmail.com>

--- cli/tink/cmd/hardware/push.go
+++ cli/tink/cmd/hardware/push.go
@@ -6,7 +6,8 @@ import (
 	"bufio"
 	"context"
 	"encoding/json"
-	"io"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -16,13 +17,33 @@ import (
 	"github.com/tinkerbell/tink/protos/hardware"
 )
 
+var (
+	filePath string
+	fPath    = "file-path"
+)
+
 // pushCmd represents the push command
 var pushCmd = &cobra.Command{
-	Use:     "push",
-	Short:   "Push new hardware to tinkerbell",
-	Example: "cat data.json | tink hardware push",
+	Use:   "push",
+	Short: "Push new hardware to tinkerbell",
+	Example: `cat /tmp/data.json | tink hardware push
+tink hardware push -p /tmp/data.json`,
+	PreRunE: func(c *cobra.Command, args []string) error {
+		if !isInputFromPipe() {
+			path, _ := c.Flags().GetString(fPath)
+			if path == "" {
+				return fmt.Errorf("%v either pipe the data or provide the required flag", c.UseLine())
+			}
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		data := readHardwareData(os.Stdin)
+		var data string
+		if isInputFromPipe() {
+			data = readDataFromStdin()
+		} else {
+			data = readDataFromFile()
+		}
 		s := struct {
 			ID string
 		}{}
@@ -38,14 +59,36 @@ var pushCmd = &cobra.Command{
 	},
 }
 
-func readHardwareData(r io.Reader) string {
-	scanner := bufio.NewScanner(bufio.NewReader(r))
+func isInputFromPipe() bool {
+	fileInfo, _ := os.Stdin.Stat()
+	return fileInfo.Mode()&os.ModeCharDevice == 0
+}
+
+func readDataFromStdin() string {
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
 	for scanner.Scan() {
 		return scanner.Text()
 	}
 	return ""
 }
 
+func readDataFromFile() string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
 func init() {
+	flags := pushCmd.PersistentFlags()
+	flags.StringVarP(&filePath, "file-path", "p", "", "path to the hardware data file")
+
 	SubCommands = append(SubCommands, pushCmd)
 }
