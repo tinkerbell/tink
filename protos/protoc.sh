ee58d5f3e5adee11f93989ac0a9ecfe8b9140cc6
parauliya <aman@infracloud.io>
2020-05-09 15:37:51 +0530

    Remove target reference from README.md and protoc.sh

--- protos/protoc.sh
+++ protos/protoc.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for proto in $(echo hardware target template workflow); do
+for proto in $(echo hardware template workflow); do
 	echo "Generating ${proto}.pb.go..."
 	protoc -I ./ -I ./common/ ${proto}/${proto}.proto --go_out=plugins=grpc:./
 done
