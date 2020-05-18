61f338cdad93971e6749aefa69d4c1c27cce664f
Manuel Mendez <manny@packet.com>
2020-05-18 12:13:01 -0400

    deploy/tf: Remove bogus defaults for token and project id
    Don't really like have invalid values in the .tf files, especially when
    Hashicorp has extensive documentation for setting variables.

--- deploy/terraform/input.tf
+++ deploy/terraform/input.tf
@@ -1,9 +1,7 @@
 variable "packet_api_token" {
   description = "Packet user api token"
-  default     = "<packet_user_api_token>"
 }
 
 variable "project_id" {
   description = "Project ID"
-  default     = "<project_id>"
 }
