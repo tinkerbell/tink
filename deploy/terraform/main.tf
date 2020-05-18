b28914cb4fb5ba283dc2e918567404948a5822eb
Manuel Mendez <manny@packet.com>
2020-05-18 12:47:09 -0400

    deploy/tf: Rename packet devices to more useful names
    s/tf-/tink-/ because this makes more sense especially if using a project
    that has other servers.

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -12,8 +12,8 @@ resource "packet_vlan" "provisioning-vlan" {
 }
 
 # Create a device and add it to tf_project_1
-resource "packet_device" "tf-provisioner" {
-  hostname         = "tf-provisioner"
+resource "packet_device" "tink-provisioner" {
+  hostname         = "tink-provisioner"
   plan             = "c3.small.x86"
   facilities       = ["sjc1"]
   operating_system = "ubuntu_18_04"
@@ -23,8 +23,8 @@ resource "packet_device" "tf-provisioner" {
 }
 
 # Create a device and add it to tf_project_1
-resource "packet_device" "tf-worker" {
-  hostname         = "tf-worker"
+resource "packet_device" "tink-worker" {
+  hostname         = "tink-worker"
   plan             = "c3.small.x86"
   facilities       = ["sjc1"]
   operating_system = "custom_ipxe"
@@ -37,22 +37,22 @@ resource "packet_device" "tf-worker" {
 
 # Attach VLAN to provisioner
 resource "packet_port_vlan_attachment" "provisioner" {
-  device_id = packet_device.tf-provisioner.id
+  device_id = packet_device.tink-provisioner.id
   port_name = "eth1"
   vlan_vnid = packet_vlan.provisioning-vlan.vxlan
 }
 
 # Attach VLAN to worker
 resource "packet_port_vlan_attachment" "worker" {
-  device_id = packet_device.tf-worker.id
+  device_id = packet_device.tink-worker.id
   port_name = "eth0"
   vlan_vnid = packet_vlan.provisioning-vlan.vxlan
 }
 
 output "provisioner_ip" {
-  value = "${packet_device.tf-provisioner.network[0].address}"
+  value = "${packet_device.tink-provisioner.network[0].address}"
 }
 
 output "worker_mac_addr" {
-  value = "${packet_device.tf-worker.ports[1].mac}"
+  value = "${packet_device.tink-worker.ports[1].mac}"
 }
