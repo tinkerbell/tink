0fa61d4c4309abb2a289519a28a2009ffd3f3e05
Graham Christensen <graham@grahamc.com>
2020-06-17 08:57:32 -0400

    proto/protoc.sh: shellcheck complaints

--- protos/protoc.sh
+++ protos/protoc.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-for proto in $(echo hardware template workflow); do
+for proto in hardware template workflow; do
 	echo "Generating ${proto}.pb.go..."
-	protoc -I ./ -I ./common/ ${proto}/${proto}.proto --go_out=plugins=grpc:./
+	protoc -I ./ -I ./common/ "${proto}/${proto}.proto" --go_out=plugins=grpc:./
 done
