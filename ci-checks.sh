2e20b07950764fd3f46e4e1cc0499cb0e7970c65
Graham Christensen <graham@grahamc.com>
2020-06-17 09:06:37 -0400

    ci-checks: run all the checks on checked-in files; check shellcheck
    Don't run on files which are not being committed, and run all
    the checks even if one step failed

--- ci-checks.sh
+++ ci-checks.sh
@@ -3,6 +3,22 @@
 
 set -eux
 
-codespell -q 3 -I .codespell-whitelist *
-prettier --check '**/*.json' '**/*.md' '**/*.yml'
-shfmt -l -d .
+failed=0
+
+if ! git ls-files | xargs codespell -q 3 -I .codespell-whitelist; then
+	failed=1
+fi
+
+if ! git ls-files '*.yml' '*.json' '*.md' | xargs prettier --check; then
+	failed=1
+fi
+
+if ! git ls-files '*.sh' | xargs shfmt -l -d; then
+	failed=1
+fi
+
+if ! git ls-files '*.sh' | xargs shellcheck; then
+	failed=1
+fi
+
+exit "$failed"
