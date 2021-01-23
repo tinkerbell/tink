f6e579d23d0aadf5f525fa39ac8d5beef28cd311
Manuel Mendez <mmendez@equinix.com>
2021-01-22 19:47:21 -0500

    db/migration: Extract migration functions out of GetMigrations
    This way we can add a test to do some basic validations of the
    migrate.Migration values.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- db/migration/migration.go
+++ db/migration/migration.go
@@ -1,16 +1,24 @@
 package migration
 
-import migrate "github.com/rubenv/sql-migrate"
+import (
+	migrate "github.com/rubenv/sql-migrate"
+)
+
+var migrations = []func() *migrate.Migration{
+	Get202009171251,
+	Get202010071530,
+	Get202010221010,
+	Get202012041103,
+	Get202012091055,
+	Get2020121691335,
+}
 
 func GetMigrations() *migrate.MemoryMigrationSource {
+	m := make([]*migrate.Migration, len(migrations))
+	for i := range migrations {
+		m[i] = migrations[i]()
+	}
 	return &migrate.MemoryMigrationSource{
-		Migrations: []*migrate.Migration{
-			Get202009171251(),
-			Get202010071530(),
-			Get202010221010(),
-			Get202012041103(),
-			Get202012091055(),
-			Get2020121691335(),
-		},
+		Migrations: m,
 	}
 }
