73e37f9596280520020af5eb093b4442f9950c51
cbkhare <Cbkhare@users.noreply.github.com>
2021-03-08 18:27:43 +0530

    docker graceful exits when image pull fails #382
    added check for Decode statement
    
    added unit tests
    
    added unit tests
    
    added unit tests
    
    added unit tests

---
+++ cmd/tink-worker/internal/registry_test.go
@@ -0,0 +1,75 @@
+package internal
+
+import (
+	"context"
+	"errors"
+	"io"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/packethost/pkg/log"
+	//"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupTestLogger(t *testing.T) log.Logger {
+	service := "github.com/tinkerbell/tink"
+	logger, err := log.Init(service)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return logger
+}
+
+type imagePullerMock struct {
+	stringReadCloser io.ReadCloser
+	imagePullErr     error
+}
+
+func (d *imagePullerMock) ImagePull(ctx context.Context, str string, op types.ImagePullOptions) (io.ReadCloser, error) {
+	return d.stringReadCloser, d.imagePullErr
+}
+
+func TestPullImageAnyFailure(t *testing.T) {
+	for _, test := range []struct {
+		testName         string
+		testString       string
+		testImagePullErr error
+		testErr          error
+	}{
+		{
+			testName:         "success",
+			testString:       "{\"status\": \"hello\",\"error\":\"\"}{\"status\":\"world\",\"error\":\"\"}",
+			testImagePullErr: nil,
+			testErr:          nil,
+		},
+		{
+			testName:         "fail",
+			testString:       "{\"error\": \"\"}",
+			testImagePullErr: errors.New("Tested, failure of the image pull"),
+			testErr:          errors.New("DOCKER PULL: Tested, failure of the image pull"),
+		},
+		{
+			testName:         "fail_partial",
+			testString:       "{\"status\": \"hello\",\"error\":\"\"}{\"status\":\"world\",\"error\":\"Tested, failure of No space left on device\"}",
+			testImagePullErr: nil,
+			testErr:          errors.New("DOCKER PULL: Tested, failure of No space left on device"),
+		},
+	} {
+		t.Run(test.testName, func(t *testing.T) {
+			ctx := context.Background()
+			rcon := NewRegistryConnDetails("test", "testUser", "testPwd", setupTestLogger(t))
+			stringReader := strings.NewReader(test.testString)
+			cli := &imagePullerMock{
+				stringReadCloser: ioutil.NopCloser(stringReader),
+				imagePullErr:     test.testImagePullErr,
+			}
+			err := rcon.pullImage(ctx, cli, test.testName)
+			if test.testErr != nil {
+				assert.Equal(t, err.Error(), test.testErr.Error())
+			}
+		})
+	}
+}
