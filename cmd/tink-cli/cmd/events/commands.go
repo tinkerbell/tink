275419eabc43ba8279ba8aa61191a9b89d8149bc
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2021-03-26 15:44:13 +0530

    delete events code
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- cmd/tink-cli/cmd/events/commands.go
+++
@@ -1,7 +0,0 @@
-package events
-
-import "github.com/spf13/cobra"
-
-// SubCommands holds the sub commands for template command
-// Example: tinkerbell template [subcommand]
-var SubCommands []*cobra.Command
