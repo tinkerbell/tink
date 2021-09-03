ad864910500b85c384701ad7513945b3b0eef6ab
Jacob Weinstock <jakobweinstock@gmail.com>
2021-09-02 21:40:46 -0600

    Remove used code:
    Clean up. Also, helps so that there are no unneeded contributions
    against these files. If the functionality in test/ is needed it
    should be moved to the sandbox repo. These are full-stack tests,
    not just Tink server tests.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- test/build_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull bash
-docker tag bash:latest localhost/bash
-docker build -t localhost/update-data actions/update_data/
-docker build -t localhost/overwrite-data actions/overwrite_data/
