dd4d994b738af928b278e50b1428479159e5c3ea
William Belcher <william.belcher@hotmail.com>
2021-09-22 11:28:37 +1000

    Fix missing packages
    Signed-off-by: William Belcher <william.belcher@hotmail.com>

--- protos/template/template.pb.go
+++ protos/template/template.pb.go
@@ -24,6 +24,8 @@ import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
