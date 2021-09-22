e94f138f6765c3e965f0ab60dfceb7b481f367c6
Thomas Stromberg <t+github@stromberg.org>
2021-09-22 11:16:15 -0700

    Remove extraneous semi-colon
    Signed-off-by: Thomas Stromberg <t+github@stromberg.org>

--- ci-checks.sh
+++ ci-checks.sh
@@ -12,7 +12,7 @@ if ! git ls-files '*.sh' '*.go' '*.md' | xargs codespell -q 3 -I .codespell-whit
 fi
 
 # --check doesn't show what line number fails, so write the result to disk for the diff to catch
-if ! git ls-files '*.yml' '*.json' '*.md' | xargs prettier --list-different --write;; then
+if ! git ls-files '*.yml' '*.json' '*.md' | xargs prettier --list-different --write; then
 	failed=1
 fi
 
