abfe05a8f79901d6fb8665219018ace60578a5ed
Manuel Mendez <mmendez@equinix.com>
2021-10-02 18:49:03 +0000

    Add buf files
    Going off of the go grpc example from buf [tour].
    
    tour: https://docs.buf.build/tour/generate-go-code/
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- tools.go
+++ tools.go
@@ -4,9 +4,10 @@ package tools
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/matryer/moq"
 	_ "github.com/stormcat24/protodep"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	_ "mvdan.cc/gofumpt"
 )
