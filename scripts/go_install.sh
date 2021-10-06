fbfa9b3dcb141c012561907412354b9d86da22ee
Micah Hausler <mhausler@amazon.com>
2021-10-06 14:29:16 -0400

    Make nixfmt happy
    Signed-off-by: Micah Hausler <mhausler@amazon.com>

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -5,28 +5,28 @@ set -o nounset
 set -o pipefail
 
 if [ -z "${1}" ]; then
-  echo "must provide module as first parameter"
-  exit 1
+	echo "must provide module as first parameter"
+	exit 1
 fi
 
 if [ -z "${2}" ]; then
-  echo "must provide binary name as second parameter"
-  exit 1
+	echo "must provide binary name as second parameter"
+	exit 1
 fi
 
 if [ -z "${3}" ]; then
-  echo "must provide version as third parameter"
-  exit 1
+	echo "must provide version as third parameter"
+	exit 1
 fi
 
 if [ -z "${GOBIN}" ]; then
-  echo "GOBIN is not set. Must set GOBIN to install the bin in a specified directory."
-  exit 1
+	echo "GOBIN is not set. Must set GOBIN to install the bin in a specified directory."
+	exit 1
 fi
 
 tmp_dir=$(mktemp -d -t goinstall_XXXXXXXXXX)
-function clean {
-  rm -rf "${tmp_dir}"
+function clean() {
+	rm -rf "${tmp_dir}"
 }
 trap clean EXIT
 
