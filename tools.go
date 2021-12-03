e2bdc5e8e868d5471a07268295a20f95aa3b4d99
Manuel Mendez <mmendez@equinix.com>
2021-12-03 15:24:04 -0500

    Add controller-gen to tools.go
    This way we can use the go tool's built in pinning functionality that
    is already used in this project.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- tools.go
+++ tools.go
@@ -9,4 +9,5 @@ import (
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	_ "mvdan.cc/gofumpt"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
