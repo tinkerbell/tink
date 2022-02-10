871d635e02c1c2fd422cb4930fa3ab9a55d57537
Manuel Mendez <mmendez@equinix.com>
2022-02-10 15:56:43 -0500

    tink-cli: Mark tinkerbell-grpc-authority as required
    It really is.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- cmd/tink-cli/cmd/root.go
+++ cmd/tink-cli/cmd/root.go
@@ -3,9 +3,11 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/tinkerbell/tink/client"
 	"github.com/tinkerbell/tink/cmd/tink-cli/cmd/internal/clientctx"
@@ -49,18 +51,37 @@ var fullClient = client.FullClient{}
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute(version string) error {
+	must := func(err error) {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to mark flag required: %+v\n", err)
+			os.Exit(1)
+		}
+	}
+
 	rootCmd.Version = version
 	rootCmd.AddCommand(NewHardwareCommand())
 	rootCmd.AddCommand(NewTemplateCommand())
 	rootCmd.AddCommand(NewWorkflowCommand())
 
 	rootCmd.PersistentFlags().StringP("facility", "f", "", "used to build grpc and http urls")
-	rootCmd.PersistentFlags().String("tinkerbell-grpc-authority", "", "Connection info for tink-server (TINKERBELL_GRPC_AUTHORITY)")
 	rootCmd.PersistentFlags().Bool("tinkerbell-tls", true, "Connect to server via TLS or not")
+
+	rootCmd.PersistentFlags().String("tinkerbell-grpc-authority", "", "Connection info for tink-server (TINKERBELL_GRPC_AUTHORITY)")
+	must(rootCmd.MarkPersistentFlagRequired("tinkerbell-grpc-authority"))
 
 	// Both AutomaticEnv and SetEnvKeyReplacer need to be called/setup before the VisitAll command that follows, otherwise env vars don't count
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
+
+	// This effectively "touches" the pflags that are marked as required if viper thinks the flag has been set
+	// This way ENV vars or values from config files can be seen as setting the required flag
+	// Really all this does is unset the required bit for any flags that have been set, therefore they are no longer required
+	rootCmd.PersistentFlags().VisitAll(func(f *pflag.Flag) {
+		if viper.IsSet(f.Name) {
+			// setting BashCompOneRequiredFlag is the opposite of MarkFlagRequired
+			_ = rootCmd.PersistentFlags().SetAnnotation(f.Name, cobra.BashCompOneRequiredFlag, []string{"false"})
+		}
+	})
 
 	_ = viper.BindPFlags(rootCmd.PersistentFlags())
 
