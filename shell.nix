6d4399453523929e6fde78d792220bab2940c381
Jacob Weinstock <jakobweinstock@gmail.com>
2022-09-15 01:54:43 +0000

    Use Go 1.18.5 to fix CI builds
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- shell.nix
+++ shell.nix
@@ -2,20 +2,32 @@ let _pkgs = import <nixpkgs> { };
 in { pkgs ? import (_pkgs.fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  #branch@date: nixos-unstable-small@2022-04-18
-  rev = "e33fe968df5a2503290682278399b1198f7ba56f";
-  sha256 = "0kr30yj9825jx4zzcyn43c398mx3l63ndgfrg1y9v3d739mfgyw3";
+  #branch@date: 21.11@2021-12-02
+  rev = "21.11";
+  sha256 = "sha256-AjhmbT4UBlJWqxY0ea8a6GU2C2HdKUREkG43oRr3TZg=";
 }) { } }:
 
 with pkgs;
 
-mkShell {
+let
+  pkgs = import (_pkgs.fetchFromGitHub {
+    # go 1.18.5
+    owner = "NixOS";
+    repo = "nixpkgs";
+    #branch@date: nixpkgs-unstable@2022-09-02
+    rev = "ee01de29d2f58d56b1be4ae24c24bd91c5380cea";
+    sha256 = "0829fqp43cp2ck56jympn5kk8ssjsyy993nsp0fjrnhi265hqps7";
+  }) { };
+
+  go_1_18_5 = pkgs.go;
+
+in mkShell {
   buildInputs = [
     docker-compose
     git
     gnumake
     gnused
-    go_1_18
+    go_1_18_5
     jq
     nixfmt
     nodePackages.prettier
